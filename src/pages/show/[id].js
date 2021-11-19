a7faa0d6e837a1188c50d6ff63e7b2e62be9433d
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-11-19 13:50:45 +0800

    added pagepreview component imports, added pagepreview bg overlay

--- src/pages/show/[id].js
+++ src/pages/show/[id].js
@@ -1,55 +1,7 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import NextImage from 'next/image'
-import { PlusIcon } from '@heroicons/react/solid'
 import Header from 'components/Header'
-import TrailerButton from 'components/TrailerButton'
 import PagePreview from 'components/PagePreview'
-
-const CoverImage = ({ img }) => {
-    return (
-        <div className='relative min-h-[calc(100vh-72px)]'>
-            <NextImage
-                src={img}
-                layout='fill'
-                objectFit='cover'
-            />
-        </div>
-    )
-}
-
-const PlayButton = () => {
-    return (
-        <button
-            className='flex items-center justify-center
-            px-6 py-2.5 rounded text-xs md:text-base border-[1px]
-            text-black bg-[#f9f9f9] hover:bg-[#c6c6c6]
-            transition duration-300 hover:border-[#c6c6c6]'
-        >
-            <img
-                src='/images/play-icon-black.svg'
-                alt=''
-                className='h-6 md:h-8'
-            />
-            <span className='uppercase font-medium tracking-wide'>
-                Play
-            </span>
-        </button>
-    )
-}
-
-const AddButton = () => {
-    return (
-        <div
-            className='w-11 h-11 rounded-full border-[1px]
-            border-white flex items-center justify-center
-            bg-black/60 cursor-pointer hover:bg-[#c6c6c6]
-            hover:border-[#c6c6c6] transition duration-300'
-        >
-            <PlusIcon className='h-6' />
-        </div>
-    )
-}
 
 export default function Show({ result }) {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'
